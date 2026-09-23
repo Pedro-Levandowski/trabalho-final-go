eb06dc36bb4b8f710528066f1dbf4bf7231b88e8
Pedro Levandowski <146142910+Pedro-Levandowski@users.noreply.github.com>
2026-09-23 18:18:18 -0300

    feat[matriculas]: adiciona matrícula e listagem de alunos por turma

--- models/turma.go
+++ models/turma.go
@@ -1,10 +1,11 @@
 package models
 
 type Turma struct {
-	ID               string `json:"id"`
-	Nome             string `json:"nome"`
-	Disciplina       string `json:"disciplina"`
-	Professor        string `json:"professor"`
-	QuantidadeAlunos int    `json:"quantidade_alunos"`
-	Alocada          bool   `json:"alocada"`
+	ID               string   `json:"id"`
+	Nome             string   `json:"nome"`
+	Disciplina       string   `json:"disciplina"`
+	Professor        string   `json:"professor"`
+	QuantidadeAlunos int      `json:"quantidade_alunos"`
+	Alocada          bool     `json:"alocada"`
+	AlunosIDs        []string `json:"-"`
 }
